a0881ecbfa5c5879c79a9872b9ee17529bcbb3d7
kristo <kristo.cisneros-valdez@my.utsa.edu>
2024-10-14 15:40:26 -0500

    added basic lerping to camera script

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,18 +1,26 @@
 extends Camera2D
 class_name CameraScript
 
-var player_node: PlayerScript = null
+const SMOOTH_MOVE_FACTOR := 3.0
+const DEFAULT_RUN_OFFSET := Vector2(50,0)
+
+@onready var player_node: PlayerScript = $"/root/test_level/Player"
+var target_node: Node2D	 = null
+var target_offset := Vector2(0,0)
+var in_camera_area := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-
+	target_node = player_node
+	global_position = target_node.global_position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if not player_node: return
+	global_position = move_toward_node(target_node, delta)	
 
-	global_position = player_node.global_position
-
+func move_toward_node(target_node: Node2D, delta):
+	var target_position = target_node.global_position + target_offset
+	return global_position.lerp(target_position, SMOOTH_MOVE_FACTOR * delta)
+	
 func set_player_node(node):
 	player_node = node

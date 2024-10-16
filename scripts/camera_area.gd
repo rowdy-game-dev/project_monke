4cc15ff05400cd7190e1cd5cff98f4d9a1a247a1
kristo <kristo.cisneros-valdez@my.utsa.edu>
2024-10-16 13:37:06 -0500

    added global_is_target bool to camera area

--- scripts/camera_area.gd
+++ scripts/camera_area.gd
@@ -3,19 +3,18 @@ extends Area2D
 @onready var player_node = $"/root/test_level/Player"
 @onready var camera_node = $"/root/test_level/camera"
 
-@export var target_position: Vector2 = self.global_position
-@export var target_zoom: Vector2 = Vector2(1,1)
+@export var global_position_is_target := true
+@export var target_position: Vector2 = Vector2()
+@export var target_zoom: Vector2 = PlayerScript.DEFAULT_PLAYER_ZOOM
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if global_position_is_target:
+		target_position = global_position
+
 	body_entered.connect(on_area_entered)
 	body_exited.connect(on_area_exited)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 func on_area_entered(body):
 	if body == player_node:
